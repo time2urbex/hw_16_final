1be01a102669e27e6854f1a74be5c5db56744a2f
time2urbex <70639041+time2urbex@users.noreply.github.com>
2022-09-11 14:46:04 +0300

    001

--- old/migrate.py
+++ old/migrate.py
@@ -10,22 +10,36 @@ def load_data(filename):
         json_data = json.load(file)
 
 
-    return json_data
-
 # Загружаем json спиок с users
 
 def load_users(filename):
-    users = load_data(filename)
+    with open('users.json', 'r', encoding='utf-8') as f:
+        users = json.load(f)
+    for user in users:
+        new_user = User(**elem)
+        db.session.add(new_user)
+        db.session.commit()
 
+"""
     for user in users:
         new_user = models.User(**user)
         db.session.add(new_user)
 
     print(models.User.get(1).to_dict)
 
+"""
+
 # Загружаем json спиок с orders
 
 def load_orders(filename):
+    with open('orders.json', 'r', encoding='utf-8') as f:
+        orders = json.load(f)
+    for order in orders:
+        new_order = Order(**elem)
+        db.session.add(new_order)
+        db.session.commit()
+
+""" 
     orders = load_data(filename)
 
     for order in orders:
@@ -33,10 +47,20 @@ def load_orders(filename):
         db.session.add(new_order)
 
     print(models.Order.get(1).to_dict)
+"""
+
 
 # Загружаем json спиок с offers
 
 def load_offers(filename):
+    with open('offers.json', 'r', encoding='utf-8') as f:
+        offers = json.load(f)
+    for offer in offers:
+        new_offer = Offer(**elem)
+        db.session.add(new_offer)
+        db.session.commit()
+
+""" 
     offers = load_data(filename)
 
     for offer in offers:
@@ -44,3 +68,4 @@ def load_offers(filename):
         db.session.add(new_offer)
 
 print(models.Offer.get(1).to_dict)
+"""
--- run.py
+++ run.py
@@ -49,54 +49,50 @@ class Order(db.Model):
 db.create_all()
 
 
+
 # Загружаем данные в json
 
 def load_data(filename):
     json_data = []
     with open(filename) as file:
         json_data = json.load(file)
-
-
     return json_data
+
 
 # Загружаем json спиок с users
 
-def load_users(filename):
-    users = load_data(filename)
-
+def load_users():
+    with open('users.json', 'r', encoding='utf-8') as f:
+        users = json.load(f)
     for user in users:
-        new_user = User(**user)
+        new_user = User(**elem)
         db.session.add(new_user)
         db.session.commit()
 
 
-load_users("users.json")
 
 # Загружаем json спиок с orders
 
-def load_orders(filename):
-    orders = load_data(filename)
-
+def load_orders():
+    with open('orders.json', 'r', encoding='utf-8') as f:
+        orders = json.load(f)
     for order in orders:
-        new_order = Order(**order)
+        new_order = Order(**elem)
         db.session.add(new_order)
         db.session.commit()
 
 
-load_orders("orders.json")
+
 
 # Загружаем json спиок с offers
 
-def load_offers(filename):
-    offers = load_data(filename)
-
+def load_offers():
+    with open('offers.json', 'r', encoding='utf-8') as f:
+        offers = json.load(f)
     for offer in offers:
-        new_offer = Offer(**offer)
+        new_offer = Offer(**elem)
         db.session.add(new_offer)
         db.session.commit()
-
-
-load_offers("offers.json")
 
 
 
